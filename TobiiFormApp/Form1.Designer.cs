98f040dd680e2daaadaeb1dfb00bb1e703dcc0e5
Kim Moon Jeong <answjd3759@gmail.com>
2023-07-20 11:31:27 +0900

    1 Euro processing

--- TobiiFormApp/Form1.Designer.cs
+++ TobiiFormApp/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
@@ -37,27 +38,41 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.radioButton4);
             this.groupBox1.Controls.Add(this.radioButton3);
             this.groupBox1.Controls.Add(this.radioButton2);
             this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Location = new System.Drawing.Point(15, 12);
+            this.groupBox1.Location = new System.Drawing.Point(17, 11);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(288, 70);
+            this.groupBox1.Size = new System.Drawing.Size(336, 114);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Gaze Smooth Filter";
             // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(7, 83);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(66, 16);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "1 EURO";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(209, 31);
+            this.radioButton3.Location = new System.Drawing.Point(175, 83);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(70, 17);
+            this.radioButton3.Size = new System.Drawing.Size(75, 16);
             this.radioButton3.TabIndex = 2;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "Unfiltered";
@@ -66,9 +81,9 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(78, 31);
+            this.radioButton2.Location = new System.Drawing.Point(175, 29);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(114, 17);
+            this.radioButton2.Size = new System.Drawing.Size(124, 16);
             this.radioButton2.TabIndex = 1;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Weighted Average";
@@ -78,9 +93,9 @@
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Checked = true;
-            this.radioButton1.Location = new System.Drawing.Point(6, 31);
+            this.radioButton1.Location = new System.Drawing.Point(7, 29);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(52, 17);
+            this.radioButton1.Size = new System.Drawing.Size(55, 16);
             this.radioButton1.TabIndex = 0;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Alpha";
@@ -90,7 +105,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 99);
+            this.label1.Location = new System.Drawing.Point(14, 166);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(89, 13);
             this.label1.TabIndex = 2;
@@ -100,7 +115,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 125);
+            this.label2.Location = new System.Drawing.Point(14, 190);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(89, 13);
             this.label2.TabIndex = 3;
@@ -109,35 +124,56 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(107, 99);
+            this.label3.Location = new System.Drawing.Point(125, 166);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.Size = new System.Drawing.Size(11, 12);
             this.label3.TabIndex = 4;
             this.label3.Text = "0";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(107, 125);
+            this.label4.Location = new System.Drawing.Point(125, 190);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.Size = new System.Drawing.Size(11, 12);
             this.label4.TabIndex = 5;
             this.label4.Text = "0";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(21, 159);
+            this.button1.Location = new System.Drawing.Point(24, 222);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(273, 57);
+            this.button1.Size = new System.Drawing.Size(318, 53);
             this.button1.TabIndex = 6;
             this.button1.Text = "Toggle gaze mouse control";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(15, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Unix Millisecond:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(139, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(11, 12);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "0";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(315, 230);
+            this.ClientSize = new System.Drawing.Size(367, 287);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -156,14 +192,17 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
+        public System.Windows.Forms.RadioButton radioButton4;
         public System.Windows.Forms.RadioButton radioButton3;
         public System.Windows.Forms.RadioButton radioButton2;
         public System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
         public System.Windows.Forms.Label label3;
         public System.Windows.Forms.Label label4;
         public System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Label label6;
     }
 }
 
--- TobiiFormApp/Program.cs
+++ TobiiFormApp/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -16,15 +17,100 @@ namespace TobiiFormApp
     {
         private static Point prevPos;
         private static bool hasPrevPos;
-        private static float alpha = 0.3f;
+        private static float alpha_smooth = 0.3f;
 
-        private enum filters {Smooth, Averaged, Unfiltered};
+        private enum filters {Smooth, Averaged, Euro, Unfiltered};
 
         private static bool enableGazeMouseControl = false;
         private static int currentFilter;
 
         private static Form1 form;
         private static IKeyboardMouseEvents m_GlobalHook;
+
+        #region EUROFilter
+        public static long GetTimestamp() // 이 값에 1000을 나눠야 second 단위임.
+        {
+            return DateTimeOffset.Now.ToUnixTimeMilliseconds();
+        }
+
+        public class OneEuroFilter
+        {
+            public OneEuroFilter(double minCutoff, double beta) 
+            {
+                firstTime = true;
+                this.minCutoff = minCutoff;
+                this.beta = beta;
+
+                xFlit = new LowpassFilter();
+                dxFlit = new LowpassFilter();
+                dcutoff = 1.0; // 여기 value 값이 초기 설정해야 하는 cutoff for derivative;
+            }
+            protected bool firstTime;
+            protected double minCutoff;
+            protected double beta;
+            protected LowpassFilter xFlit;
+            protected LowpassFilter dxFlit;
+            protected double dcutoff;
+
+            public double MinCutoff
+            {
+                get { return minCutoff; }
+                set { minCutoff = value; } //여기 value 값이 초기 설정해야 하는 cutoff minimum;
+            }
+            public double Beta
+            {
+                get { return beta; }
+                set { beta = value; } //여기 value 값이 초기 설정해야 하는 beta;
+            }
+            public double Filter(double x, double rate)
+            {
+                double dx = firstTime ? 0 : (x - xFlit.Last()) * rate;
+                if (firstTime)
+                {
+                    firstTime = false;
+                }
+                var edx = dxFlit.Filter(dx, Alpha(rate, dcutoff));
+                var cutoff = minCutoff + beta * Math.Abs(edx);
+
+                return xFlit.Filter(x, Alpha(rate, cutoff));
+            }
+            protected double Alpha(double rate, double cutoff)
+            {
+                var tau = 1.0 / (2 * Math.PI * cutoff);
+                var te = 1.0 / rate;
+                return 1.0 / (1.0 + tau / te);
+            }
+        }
+        public class LowpassFilter
+        {
+            public LowpassFilter()
+            {
+                firstTime = true;
+            }
+            protected bool firstTime;
+            protected double hatXPrev;
+
+            public double Last()
+            {
+                return hatXPrev;
+            }
+            public double Filter(double x, double alpha)
+            {
+                double hatX = 0;
+                if(firstTime)
+                {
+                    firstTime = false;
+                    hatX = x;
+                }
+                else
+                {
+                    hatX = alpha * x + (1 - alpha) * hatXPrev;
+                }
+                hatXPrev = hatX;
+                return hatX;
+            }
+        }
+        #endregion
 
         private static void GlobalHookKeyDown(object sender, KeyEventArgs e)
         {
@@ -80,13 +166,18 @@ namespace TobiiFormApp
 
             if(currentFilter == (int)filters.Smooth)
             {
-                filteredPoint = new Point((int)((point.X * alpha) + (prevPos.X * (1.0f - alpha))),
-                                                (int)((point.Y * alpha) + (prevPos.Y * (1.0f - alpha))));
-            }else if(currentFilter == (int)filters.Averaged)  //takes the average of the current point and the previous point
+                filteredPoint = new Point((int)((point.X * alpha_smooth) + (prevPos.X * (1.0f - alpha_smooth))),
+                                                (int)((point.Y * alpha_smooth) + (prevPos.Y * (1.0f - alpha_smooth))));
+            }
+            else if(currentFilter == (int)filters.Averaged)  //takes the average of the current point and the previous point
             {
                 filteredPoint = new Point((point.X + prevPos.X) / 2, (point.Y + prevPos.Y) / 2);
             }
-
+            else if(currentFilter == (int)filters.Euro)
+            {
+                OneEuroFilter oneEuroFilter
+                filteredPoint = new Point(point.X, point.Y);
+            }
             prevPos = filteredPoint; //set the previous point to current point
 
             return filteredPoint;
@@ -106,6 +197,10 @@ namespace TobiiFormApp
             else if (form.radioButton2.Checked) //Averaged filter
             {
                 currentFilter = (int)filters.Averaged;
+            }
+            else if (form.radioButton4.Checked) // 1EURO filter
+            {
+                currentFilter = (int)filters.Euro;
             }
             else //unfiltered
             {
@@ -138,6 +233,7 @@ namespace TobiiFormApp
                     //update the form labels with gaze coordinate
                     form.label3.Invoke((MethodInvoker)(() => form.label3.Text = x.ToString()));
                     form.label4.Invoke((MethodInvoker)(() => form.label4.Text = y.ToString()));
+                    form.label6.Invoke((MethodInvoker)(() => form.label6.Text = GetTimestamp().ToString())); 
                 }
                 
             });
